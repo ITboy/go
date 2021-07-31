6159e5c64094abd0474be2ad26e82b1ee5b62b65
prepper <prepper@126.com>
2021-07-31 16:03:38 +0800

    add postAlbum handler

--- demo/web-service-rest/main.go
+++ demo/web-service-rest/main.go
@@ -19,12 +19,22 @@ var albums = []album{
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
-func getAlmbus(c *gin.Context) {
+func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbum(c *gin.Context) {
+	var newAlbum album
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlmbus)
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbum)
 	router.Run("localhost:8080")
 }
